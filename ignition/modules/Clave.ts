9f19d4d78fc5ff1ce5d835eb7ff4f1de1978475e
Davison Mota <davimotagnr@gmail.com>
2025-09-12 12:04:33 -0300

    config ClaveModule

--- ignition/modules/Clave.ts
+++ ignition/modules/Clave.ts
@@ -3,6 +3,9 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 export default buildModule("ClaveModule", (m) => {
 
   // TODO: Set addresses for the contract arguments below
+  const defaultAdmin = m.getAccount(0); // primeira conta do Hardhat
+  const minter = m.getAccount(1); // segunda conta do Hardhat
+
   const clave = m.contract("Clave", [defaultAdmin, minter]);
 
   return { clave };
